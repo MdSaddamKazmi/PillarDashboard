5d9e1b60e92cfc14440e20c37bb3ad844d049995
MdSaddamKazmi <54942497+MdSaddamKazmi@users.noreply.github.com>
2021-07-13 14:28:26 +0530

    Update App.controller.js

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -33,43 +33,6 @@ sap.ui.define([
 		onInit: function () {
 
 			var oModel1 = new sap.ui.model.json.JSONModel({
-				Skills: [{
-						"Technical_Skill": "Health Pillar",
-						"Value": "45"
-					}, {
-						"Technical_Skill": "Innotech Pillar",
-						"Value": "68"
-					}, {
-						"Technical_Skill": "People Pillar",
-						"Value": "35"
-					}, {
-						"Technical_Skill": "Social Pillar",
-						"Value": "10"
-					}, {
-						"Technical_Skill": "Business Pillar",
-						"Value": "20"
-					}
-
-				],
-
-				Skills1: [{
-						"Technical_Skill": "Health Pillar",
-						"Value": "30"
-					}, {
-						"Technical_Skill": "Innotech Pillar",
-						"Value": "40"
-					}, {
-						"Technical_Skill": "People Pillar",
-						"Value": "50"
-					}, {
-						"Technical_Skill": "Social Pillar",
-						"Value": "60"
-					}, {
-						"Technical_Skill": "Business Pillar",
-						"Value": "70"
-					}
-
-				],
 
 				Health: [{
 						"Event_Name": "Dance sessions",
@@ -130,13 +93,68 @@ sap.ui.define([
 						"Number_of_times": "2"
 					}
 
+				],
+
+				Home: [{
+
+						"Event_Name": "Dance sessions",
+						"Number_of_times": "4"
+					}, {
+						"Event_Name": "Yoga Sessions",
+						"Number_of_times": "2"
+					}, {
+						"Event_Name": "Walk Challenge",
+						"Number_of_times": "3"
+
+					},
+
+					{
+						"Event_Name": "Demo Sessions",
+						"Number_of_times": "4"
+					}, {
+						"Event_Name": "Online Learning Announcement",
+						"Number_of_times": "3"
+					}, {
+						"Event_Name": "Innovative Challenge",
+						"Number_of_times": "1"
+					},
+
+					{
+						"Event_Name": "R&R",
+						"Number_of_times": "1"
+					}, {
+						"Event_Name": "Online Games",
+						"Number_of_times": "3"
+					},
+
+					{
+						"Event_Name": "Online Classes",
+						"Number_of_times": "2"
+					}, {
+						"Event_Name": "Goodies Distribution",
+						"Number_of_times": "1"
+					},
+
+					{
+						"Event_Name": "News Letter",
+						"Number_of_times": "2"
+					}, {
+						"Event_Name": "Coffee Corners",
+						"Number_of_times": "3"
+					},
+
+					{
+						"Event_Name": "Buzz Shots Videos",
+						"Number_of_times": "2"
+					}
+
 				]
 
 			});
 
 			this.getView().setModel(oModel1);
 
-			this.byId("PillarCombo").setSelectedKey("Health");
+			this.byId("PillarCombo").setSelectedKey("Home");
 
 			// //      1.Get the id of the VizFrame		
 			// var oVizFrame = this.getView().byId("idcolumn");
@@ -230,40 +248,89 @@ sap.ui.define([
 			var Pillar = this.byId("PillarCombo").getSelectedKey();
 
 			switch (Pillar) {
+			case "Home":
+				this.byId("blockHome").setVisible(true);
+				this.byId("blockHealth").setVisible(false);
+				this.byId("blockInnotech").setVisible(false);
+				this.byId("blockPeople").setVisible(false);
+				this.byId("blockSocial").setVisible(false);
+				this.byId("blockBusiness").setVisible(false);
+				this.byId("table").setVisible(true);
+				this.byId("table1").setVisible(false);
+				this.byId("table2").setVisible(false);
+				this.byId("table3").setVisible(false);
+				this.byId("table4").setVisible(false);
+				this.byId("table5").setVisible(false);
+				break;
 			case "Health":
+				this.byId("blockHome").setVisible(false);
 				this.byId("blockHealth").setVisible(true);
 				this.byId("blockInnotech").setVisible(false);
 				this.byId("blockPeople").setVisible(false);
 				this.byId("blockSocial").setVisible(false);
 				this.byId("blockBusiness").setVisible(false);
+				this.byId("table").setVisible(false);
+				this.byId("table1").setVisible(true);
+				this.byId("table2").setVisible(false);
+				this.byId("table3").setVisible(false);
+				this.byId("table4").setVisible(false);
+				this.byId("table5").setVisible(false);
 				break;
 			case "Innotech":
+				this.byId("blockHome").setVisible(false);
 				this.byId("blockHealth").setVisible(false);
 				this.byId("blockInnotech").setVisible(true);
 				this.byId("blockPeople").setVisible(false);
 				this.byId("blockSocial").setVisible(false);
 				this.byId("blockBusiness").setVisible(false);
+				this.byId("table").setVisible(false);
+				this.byId("table1").setVisible(false);
+				this.byId("table2").setVisible(true);
+				this.byId("table3").setVisible(false);
+				this.byId("table4").setVisible(false);
+				this.byId("table5").setVisible(false);
 				break;
 			case "People":
+				this.byId("blockHome").setVisible(false);
 				this.byId("blockHealth").setVisible(false);
 				this.byId("blockInnotech").setVisible(false);
 				this.byId("blockPeople").setVisible(true);
 				this.byId("blockSocial").setVisible(false);
 				this.byId("blockBusiness").setVisible(false);
+				this.byId("table").setVisible(false);
+				this.byId("table1").setVisible(false);
+				this.byId("table2").setVisible(false);
+				this.byId("table3").setVisible(true);
+				this.byId("table4").setVisible(false);
+				this.byId("table5").setVisible(false);
 				break;
 			case "Social":
+				this.byId("blockHome").setVisible(false);
 				this.byId("blockHealth").setVisible(false);
 				this.byId("blockInnotech").setVisible(false);
 				this.byId("blockPeople").setVisible(false);
 				this.byId("blockSocial").setVisible(true);
 				this.byId("blockBusiness").setVisible(false);
+				this.byId("table").setVisible(false);
+				this.byId("table1").setVisible(false);
+				this.byId("table2").setVisible(false);
+				this.byId("table3").setVisible(false);
+				this.byId("table4").setVisible(true);
+				this.byId("table5").setVisible(false);
 				break;
 			case "Business":
+				this.byId("blockHome").setVisible(false);
 				this.byId("blockHealth").setVisible(false);
 				this.byId("blockInnotech").setVisible(false);
 				this.byId("blockPeople").setVisible(false);
 				this.byId("blockSocial").setVisible(false);
 				this.byId("blockBusiness").setVisible(true);
+				this.byId("table").setVisible(false);
+				this.byId("table1").setVisible(false);
+				this.byId("table2").setVisible(false);
+				this.byId("table3").setVisible(false);
+				this.byId("table4").setVisible(false);
+				this.byId("table5").setVisible(true);
 
 			}
 
